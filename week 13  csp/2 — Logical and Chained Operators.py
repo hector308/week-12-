3a05f61e3858e9fc722eb5c3dacf4cfd99fa008c
hector308 <hecotrm2344@gmail.com>
2025-11-12 15:31:23 +0000

    loool

--- week 13  csp/2 — Logical and Chained Operators.py
+++ week 13  csp/2 — Logical and Chained Operators.py
@@ -18,14 +18,18 @@ print(x < 5 or x == 10)   # True
 print(not(x == 10))       # False
 print(1 < x < 20)         # True
 
-
+print("!")
 # Practice Problems:
 
 # Write an expression that checks if a number is between 50 and 100 (inclusive).
-
+y=60
+print(y>=50 and y<=100)
 # Write an expression that checks if a number is NOT equal to 0 and greater than 10.
-
+z=7
+print(not(z==0 and z>=10))
 # Use chained comparison to check if 3 < 4 < 5.
+c=4
+print (3<c<5)
 
 # Challenge: Create a password rule using logical operators:
 
